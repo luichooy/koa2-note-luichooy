b4d5d73e1789e01fde62240b5a172bf48d148c63
luichooy <luichooy@163.com>
2019-07-05 18:08:52 +0800

    use nodemon

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(async ctx => {
-  ctx.body = 'Hello Koa'
+  ctx.body = 'Hello Koa!'
 })
 
 app.listen(3000)
 
-console.log('App is runnig at port 3000')
+console.log('App is runnig at port 3000.')
